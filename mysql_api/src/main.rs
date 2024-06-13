48617637da4b6cfe7cfa918d123e70e5cde3b458
dongyul1u <liu.dongyu@northeastern.edu>
2024-06-13 23:29:11 +0800

    news db api

--- mysql_api/src/main.rs
+++ mysql_api/src/main.rs
@@ -11,8 +11,8 @@ use rocket::figment::Figment;
 mod db;
 mod model;
 use {
-    db::{ get_blocks, get_mysql_connection},
-    model::BlockData,
+    db::{ get_blocks, get_mysql_connection, get_news},
+    model::{BlockData, NewsData},
 };
 
 #[get("/blocks")]
@@ -26,6 +26,16 @@ fn get_blocks_handler() -> Json<Vec<BlockData>> {
     }
 }
 
+#[get("/news")]
+fn get_news_handler() -> Json<Vec<NewsData>> {
+    let pool = get_mysql_connection().expect("Failed to get MySQL connection pool");
+    let mut conn = pool.get_conn().expect("Failed to get connection from pool");
+
+    match get_news(&mut conn) {
+        Ok(news) => Json(news),
+        Err(_) => Json(vec![]), // return empty if error
+    }
+}
 
 #[launch]
 fn rocket() -> _ {
@@ -33,5 +43,5 @@ fn rocket() -> _ {
         .merge(("port", 8080)); // Set port to 8080
 
     rocket::custom(figment)
-        .mount("/", routes![get_blocks_handler])
+        .mount("/", routes![get_blocks_handler, get_news_handler])
 }
--- mysql_api/src/model.rs
+++ mysql_api/src/model.rs
@@ -2,5 +2,21 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct BlockData {
+    pub hash: String, // hash ID
+    pub time: String,  //Unix time as timestamp
+    pub block_index: usize,   //index in whole chain
     pub height: usize,    //height in whole chain
+    pub fee: usize,     // operation fee for the miner
+    pub n_tx: usize,    //transcations num
+    // pub transcation: Vec<TransAction>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct NewsData {
+    pub id: String,
+    pub title: String,
+    pub url: String,
+    pub body: String,
+    pub source: String,
+    pub tags: String
 }
